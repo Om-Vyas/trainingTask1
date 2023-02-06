289da2fd005b98a0d31b660ee337d8934fa756c3
Om Vyas <vyasom2691@gmail.com>
2023-02-06 16:58:16 +0530

    render data through JSON.

---
+++ courses.js
@@ -0,0 +1,15 @@
+export const coursedata = [
+  {
+    star: true,
+    courseImage: "./assets/images/imageMask.png",
+    courseTitle: "Acceleration",
+    courseGrade: ["Grade 7", "+2"],
+    courseUnit: [4, 18, 24],
+    courseClasses: ["Mr. Frank's Class B"],
+    courseStudent: 50,
+    coursedate: {
+      from: "21-Jan-2020",
+      to: "21-&nbsp;Aug-2020",
+    },
+  },
+];
